433369c4ac5a2e176dabd47d24f27a6f52efbc5a
farahatqah <farahatqah@gmail.com>
2021-10-26 02:25:53 +0800

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGWqdA7qfKOnOBEuo_08nG7YUZM54znTI",
+    authDomain: "e-ducate-me.firebaseapp.com",
+    projectId: "e-ducate-me",
+    storageBucket: "e-ducate-me.appspot.com",
+    messagingSenderId: "354977666843",
+    appId: "1:354977666843:web:c799cf49ed43c013be25be",
+    measurementId: "G-7EXJMT3JM0"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
